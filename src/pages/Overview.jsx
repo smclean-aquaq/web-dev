d8476c9333c9336b2083ce7eaddfa24d05561c12
cremurtagh <criomhthann.murtagh@aquaq.co.uk>
2022-11-02 13:51:55 +0000

    jam sandwich

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -69,7 +69,7 @@ const Overview = () => {
       <div className="flex gap-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Updates</p>
+            <p className="font-semibold text-xl">Jam Sandwich, Extra Jam</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
                 <span>
